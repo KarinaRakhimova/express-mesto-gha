66635fd8daed3218c16499d5039f0a9e43badd9b
KarinaRakhimova <rahimova@list.ru>
2023-04-30 20:53:16 +0500

    9

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError.js');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const auth = (req, res, next) => {
   const { cookie } = req.headers;
