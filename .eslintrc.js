520de8e955b04340c0631697368da08f71df8b8b
KarinaRakhimova <rahimova@list.ru>
2023-05-02 22:39:38 +0500

    eslintrc fx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,5 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-  },
   extends: ['eslint:recommended', 'airbnb-base'],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
   rules: {
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
     'no-useless-escape': 'off',
