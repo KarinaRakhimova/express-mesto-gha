d2b32360aa3268b4691116f1125db70bfa1f9c13
KarinaRakhimova <rahimova@list.ru>
2023-05-02 14:58:15 +0500

    fx after review try5

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ const auth = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(token, 'testeee2@test.ru');
+    payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
