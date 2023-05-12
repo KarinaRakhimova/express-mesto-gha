97168ac582af6c0b0b5bc68429bd4b5319602399
KarinaRakhimova <rahimova@list.ru>
2023-05-12 15:08:23 +0500

    1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,9 @@ const cors = require('cors');
 
 const allowedCors = [
   'https://mesto2023.nomoredomains.monster',
+  'http://mesto2023.nomoredomains.monster',
   'https://api.mesto2023.students.nomoredomains.monster',
+  'http://api.mesto2023.students.nomoredomains.monster',
   'https://51.250.64.58',
   'http://localhost:3000',
   'http://localhost:3001',
