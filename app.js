5bba0a121dc4d2fda4677fbfa9182607f461a9d9
KarinaRakhimova <rahimova@list.ru>
2023-05-12 15:27:50 +0500

    3

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.options('*', cors({
-  origin: ['http://mesto2023.nomoredomains.monster', 'https://mesto2023.nomoredomains.monster'],
-  credentials: true,
-}));
-app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
+// app.options('*', cors({
+//   origin: ['http://mesto2023.nomoredomains.monster', 'https://mesto2023.nomoredomains.monster'],
+//   credentials: true,
+// }));
 // app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(requestLogger);
 app.use('/', indexRouter);
