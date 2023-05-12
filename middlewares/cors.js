8a9dbceaf883cdcc0f828c948ccbcc776ba7a5d5
KarinaRakhimova <rahimova@list.ru>
2023-05-12 10:20:05 +0500

    fx cors7

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -12,7 +12,9 @@ const cors = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
