8cfa6aa64b078278be7087a3375b3eb27219f195
KarinaRakhimova <rahimova@list.ru>
2023-05-15 00:12:05 +0500

    13

--- app.js
+++ app.js
@@ -22,18 +22,19 @@ const allowedCors = [
 const app = express();
 app.use(cookieParser());
 app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
 
+  const { host } = req.headers;
+  console.log('origin', host);
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Origin', 'https://mesto2023.nomoredomains.monster');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
+  }
+  if (allowedCors.includes(host)) {
+    res.header('Access-Control-Allow-Origin', host);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   next();
 });
