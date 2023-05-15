95919f8d0af088b28b0143a76dfd9f210b2a6b31
KarinaRakhimova <rahimova@list.ru>
2023-05-15 09:41:01 +0500

    18

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
   }
   if (allowedCors.includes(origin)) {
